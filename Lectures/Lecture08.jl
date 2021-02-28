59af524b1a02923ca32fb765de6912e9968ab7d7
Ben Lauwens <ben.lauwens@gmail.com>
2021-02-28 21:27:19 +0100

    Add Lecture09

--- Lectures/Lecture08.jl
+++ Lectures/Lecture08.jl
@@ -255,6 +255,426 @@ julia> greeting = "J" * greeting[2:end]
 This example concatenates a new first letter onto a slice of `greeting`. It has no effect on the original string.
 """
 
+# ╔═╡ e6a4882e-79fc-11eb-3957-95357985303f
+md"""## String Interpolation
+
+Constructing strings using concatenation can become a bit cumbersome. To reduce the need for these verbose calls to string or repeated multiplications, Julia allows *string interpolation* using `$`:
+
+```julia
+julia> greet = "Hello" 
+"Hello"
+julia> whom = "World" 
+"World"
+julia> "$greet, $(whom)!" 
+"Hello, World!"
+```
+
+This is more readable and convenient than string concatenation:
+
+```julia
+greet * ", " * whom * "!"
+```
+
+The shortest complete expression after the `$` is taken as the expression whose value is to be interpolated into the string. Thus, you can interpolate any expression into a string using parentheses:
+
+```julia
+julia> "1 + 2 = $(1 + 2)" 
+"1 + 2 = 3"
+```
+"""
+
+# ╔═╡ 36ac2624-79fd-11eb-1099-910ca7cea1c9
+md"""## Searching
+What does the following function do?
+
+```julia
+function find(word, letter) 
+	index = firstindex(word) 
+	while index <= sizeof(word)
+		if word[index] == letter 
+			return index
+		end
+		index = nextind(word, index) 
+	end
+	-1 
+end
+```
+
+In a sense, `find` is the inverse of the `[]` operator. Instead of taking an index and extracting the corresponding character, it takes a character and finds the index where that character appears. If the character is not found, the function returns `-1`.
+
+
+This is the first example we have seen of a `return` statement inside a loop. If `word[index] == letter`, the function breaks out of the loop and returns immediately.
+
+If the character doesn’t appear in the string, the program exits the loop normally and returns `-1`.
+
+This pattern of computation—traversing a sequence and returning when we find what we are looking for—is called a *search*.
+"""
+
+# ╔═╡ e04750c8-79fd-11eb-0249-ed3fb7e031b2
+md"""#### Exercise 8-4
+
+Modify `find` so that it has a third parameter, the index in word where it should start looking.
+"""
+
+# ╔═╡ 019a3968-79fe-11eb-206a-3d92d9352524
+md"""## Looping and Counting
+
+The following program counts the number of times the letter a appears in a string:
+
+```julia
+word = "banana" 
+counter = 0
+for letter in word
+	if letter == 'a'
+		counter = counter + 1
+	end 
+end
+println(counter)
+```
+
+This program demonstrates another pattern of computation called a *counter*. The variable `counter` is initialized to `0` and then incremented each time an `a` is found. When the loop exits, counter contains the result—the total number of `a`’s.
+"""
+
+# ╔═╡ 5af18f32-79fe-11eb-1860-0f08e3d25a3f
+md"""#### Exercise 8-5
+
+Encapsulate this code in a function named `count`, and generalize it so that it accepts the string and the letter as arguments.
+
+Then rewrite the function so that instead of traversing the string, it uses the three parameter version of `find` from the previous section.
+"""
+
+# ╔═╡ 8d5f176e-79fe-11eb-29fc-813db53b35a6
+md"""## String Library
+
+Julia provides functions that perform a variety of useful operations on strings. For example, the function `uppercase` takes a string and returns a new string with all uppercase letters:
+
+```julia
+julia> uppercase("Hello, World!") 
+"HELLO, WORLD!"
+```
+
+As it turns out, there is a function named `findfirst` that is remarkably similar to the `find` function we wrote:
+
+```julia
+julia> findfirst("a", "banana") 
+2:2
+```
+
+Actually, the `findfirst` function is more general than our function; it can find sub‐ strings, not just characters:
+
+```julia
+julia> findfirst("na", "banana") 
+3:4
+```
+
+By default, `findfirst` starts at the beginning of the string, but the function `findnext` takes a third argument, the index where it should start looking:
+
+```julia
+julia> findnext("na", "banana", 4) 
+5:6
+```
+"""
+
+# ╔═╡ e80e049a-79fe-11eb-2ade-83a99e44e5a5
+md"""## The `∈` Operator
+
+The operator `∈` (**`\in TAB`**) is a Boolean operator that takes a character and a string and returns `true` if the first appears in the second: 
+
+```julia
+julia> 'a' ∈ "banana" # 'a' in "banana"
+true
+```
+
+For example, the following function prints all the letters from `word1` that also appear in `word2`:
+
+```julia
+function inboth(word1, word2) 
+	for letter in word1
+		if letter ∈ word2 
+			print(letter, " ")
+		end 
+	end
+end
+```
+
+With well-chosen variable names, Julia sometimes reads like English. You could read this loop as “for (each) letter in (the first) word, if (the) letter is an element of (the second) word, print (the) letter.”
+
+Here’s what you get if you compare `"apples"` and `"oranges"`: 
+
+```julia
+julia> inboth("apples", "oranges")
+a e s
+```
+"""
+
+# ╔═╡ 5487357e-79ff-11eb-2492-d35dfed491cc
+md"""## String Comparison
+
+The relational operators work on strings. To see if two strings are equal, use `==`:
+
+```julia
+word = "Pineapple" 
+if word == "banana"
+	println("All right, bananas.") 
+end
+```
+
+Other relational operations are useful for putting words in alphabetical order:
+
+```julia
+if word < "banana"
+	println("Your word, $word, comes before banana.")
+elseif word > "banana"
+	println("Your word, $word, comes after banana.")
+else
+	println("All right, bananas.")
+end
+```
+
+Julia does not handle uppercase and lowercase letters the same way people do. All the uppercase letters come before all the lowercase letters, so:
+
+```
+Your word, Pineapple, comes before banana.
+```
+
+!!! tip
+    A common way to address this problem is to convert strings to a standard format, such as all lowercase, before performing the comparison.
+"""
+
+# ╔═╡ a05f7e2a-79ff-11eb-16f9-db358e178910
+md"""## Debugging
+
+When you use indices to traverse the values in a sequence, it is tricky to get the beginning and end of the traversal right. Here is a function that is supposed to compare two words and return `true` if one of the words is the reverse of the other, but it contains two errors:
+
+```julia
+function isreverse(word1, word2)
+	if length(word1) != length(word2)
+		return false
+    end
+	i = firstindex(word1) 
+	j = lastindex(word2) 
+	while j >= 0
+		j = prevind(word2, j) 
+		if word1[i] != word2[j]
+			return false
+		end
+		i = nextind(word1, i) 
+	end
+	true 
+end
+```
+
+The first `if` statement checks whether the words are the same length. If not, we can return `false` immediately. Otherwise, for the rest of the function, we can assume that the words are the same length. This is an example of the guardian pattern; see “Checking Types”.
+
+`i` and `j` are indices: `i` traverses `word1` forward while `j` traverses `word2` backward. If we find two letters that don’t match, we can return false immediately. If we get through the whole loop and all the letters match, we return `true`.
+
+The function `lastindex` returns the last valid byte index of a string and `prevind` finds the previous valid index of a character.
+
+If we test this function with the words `“pots”` and `“stop”`, we expect the return value `true`, but we get `false`:
+
+```julia
+julia> isreverse("pots", "stop") 
+false
+```
+
+For debugging this kind of error, my first move is to print the values of the indices:
+
+```julia
+while j >= 0
+	j = prevind(word2, j)
+	@show i j
+	if word1[i] != word2[j]
+```
+
+Now when I run the program again, I get more information:
+
+```julia
+julia> isreverse("pots", "stop") 
+i = 1
+j = 3
+false
+```
+
+The first time through the loop, the value of `j` is `3`, but it has to be `4`. This can be fixed by moving `j = prevind(word2, j)` to the end of the `while` loop.
+
+If I fix that error and run the program again, I get:
+
+```julia
+julia> isreverse("pots", "stop") 
+i = 1
+j = 4
+i = 2
+j = 3
+i = 3
+j = 2
+i = 4
+j = 1
+i = 5
+j = 0
+ERROR: BoundsError: attempt to access "pots"
+at index [5]
+```
+
+This time a `BoundsError` has been thrown. The value of `i` is `5`, which is out of range for the string `"pots"`.
+"""
+
+# ╔═╡ c657df0e-7a00-11eb-06e7-b1839a01a3b1
+md"""#### Exercise 8-6
+
+Run the program on paper, changing the values of `i` and `j` during each iteration. Find and fix the second error in this function.
+"""
+
+# ╔═╡ e8baffa4-7a00-11eb-3373-3d1dc1e6a952
+md"""## Glossary
+
+*sequence*:
+An ordered collection of values where each value is identified by an integer index.
+
+*ASCII standard*:
+A character encoding standard for electronic communication specifying 128 characters.
+
+*Unicode standard*:
+A computing industry standard for the consistent encoding, representation, and handling of text expressed in most of the world’s writing systems.
+
+*index*:
+An integer value used to select an item in a sequence, such as a character in a string. In Julia indices start from 1.
+
+*UTF-8 encoding*:
+A variable-width character encoding capable of encoding all 1,112,064 valid code points in Unicode using one to four 8-bit bytes.
+
+*traverse*:
+To iterate through the items in a sequence, performing a similar operation on each.
+slice
+
+*empty string*:
+A string with no characters and length 0, represented by two quotation marks.
+
+*immutable*:
+The property of a sequence whose items cannot be changed.
+
+*string interpolation*:
+The process of evaluating a string containing one or more placeholders, yielding a result in which the placeholders are replaced with their corresponding values.
+
+*search*:
+A pattern of traversal that stops when it finds what it is looking for.
+
+*counter*:
+A variable used to count something, usually initialized to zero and then incremented.
+"""
+
+# ╔═╡ 55115388-7a01-11eb-2a9e-a769127bc90d
+md"""## Exercises 
+
+#### Exercise 8-7
+
+Read the [documentation of the string functions](https://docs.julialang.org/en/v1/manual/strings/). You might want to experiment with some of them to make sure you understand how they work. `strip` and `replace` are particularly useful.
+
+
+The documentation uses a syntax that might be confusing. For example, in `search(string::AbstractString, chars::Chars, [start::Integer])`, the brackets indicate optional arguments. So, `string` and `chars` are required, but `start` is optional.
+"""
+
+# ╔═╡ bcb82726-7a01-11eb-1b75-7136ad9adfb6
+md"""#### Exercise 8-8
+
+There is a built-in function called count that is similar to the function in “Looping and Counting”. Read the documentation of this function and use it to count the number of `a`’s in `"banana"`.
+"""
+
+# ╔═╡ d33a25a0-7a01-11eb-35cc-573b861aadbb
+md"""#### Exercise 8-9
+
+A string slice can take a third index. The first specifies the start, the third the end, and the second the “step size”; that is, the number of spaces between successive characters. A step size of 2 means every other character; 3 means every third, etc. For example:
+
+```julia
+julia> fruit = "banana" 
+"banana"
+julia> fruit[1:2:6] 
+"bnn"
+```
+
+A step size of `-1` goes through the word backward, so the slice `[end:-1:begin]` generates a reversed string.
+
+Use this idiom to write a one-line version of `ispalindrome` from “Exercise 6-6”.
+"""
+
+# ╔═╡ 0d718e70-7a02-11eb-27e0-0f145b163d55
+md"""#### Exercise 8-10
+
+The following functions are all *intended* to check whether a string contains any lowercase letters, but at least some of them are wrong. For each function, describe what the function actually does (assuming that the parameter is a string).
+
+```julia
+function anylowercase1(s) 
+	for c in s
+		if islowercase(c) 
+			return true
+		else
+			return false
+		end 
+	end
+end
+
+function anylowercase2(s) 
+	for c in s
+		if islowercase('c') 
+			return "true"
+		else
+			return "false"
+		end 
+	end
+end
+
+function anylowercase3(s)
+	for c in s
+		flag = islowercase(c) 
+	end
+	flag
+end
+
+function anylowercase4(s) 
+	flag = false
+	for c in s
+		flag = flag || islowercase(c)
+	end
+	flag
+end
+
+function anylowercase5(s) 
+	for c in s
+		if !islowercase(c) 
+			return false
+		end 
+	end
+	true 
+end
+```
+"""
+
+# ╔═╡ 7e1acfec-7a02-11eb-2b50-ff3f9e93545e
+md"""#### Exercise 8-11
+
+A Caesar cypher is a weak form of encryption that involves “rotating” each letter by a fixed number of places. To rotate a letter means to shift it through the alphabet, wrap‐ ping around to the beginning if necessary, so *`A`* rotated by 3 is *`D`* and *`Z`* rotated by 1 is *`A`*.
+
+To rotate a word, rotate each letter by the same amount. For example, `"cheer"` rota‐ ted by 7 is `"jolly"` and `"melon"` rotated by –10 is `"cubed"`. In the movie *2001: A Space Odyssey*, the ship’s computer is called “HAL,” which is “IBM” rotated by –1.
+
+Write a function called `rotateword` that takes a string and an integer as parameters, and returns a new string that contains the letters from the original string rotated by the given amount.
+
+!!! tip
+    You might want to use the built-in functions `Int`, which converts a character to a numeric code, and `Char`, which converts numeric codes to characters. Letters of the alphabet are encoded in alphabetical order, so, for example:
+    
+    ```julia
+    julia> Int('c') - Int('a') 
+    2
+    ```
+    
+    because *c* is the third letter of the alphabet. But beware—the numeric codes for uppercase letters are different:
+    
+    ```julia
+    julia> Char(Int('A') + 32)
+    'a': ASCII/Unicode U+0061 (category Ll: Letter, lower case)
+    ```
+
+Potentially offensive jokes on the internet are sometimes encoded in ROT13, which is a Caesar cypher with rotation 13. If you are not easily offended, find and decode some of them.
+"""
+
 # ╔═╡ Cell order:
 # ╟─2444e25e-76e3-11eb-021a-e9e976b267bb
 # ╟─d77c9e58-76e2-11eb-27a0-23914499f29f
@@ -268,3 +688,19 @@ This example concatenates a new first letter onto a slice of `greeting`. It has 
 # ╟─c170dc56-76e6-11eb-2bd1-050911e1915f
 # ╟─559c8a42-76e7-11eb-371c-391c5deba644
 # ╟─67110424-76e7-11eb-14c3-e195649e6d41
+# ╟─e6a4882e-79fc-11eb-3957-95357985303f
+# ╟─36ac2624-79fd-11eb-1099-910ca7cea1c9
+# ╟─e04750c8-79fd-11eb-0249-ed3fb7e031b2
+# ╟─019a3968-79fe-11eb-206a-3d92d9352524
+# ╟─5af18f32-79fe-11eb-1860-0f08e3d25a3f
+# ╟─8d5f176e-79fe-11eb-29fc-813db53b35a6
+# ╟─e80e049a-79fe-11eb-2ade-83a99e44e5a5
+# ╟─5487357e-79ff-11eb-2492-d35dfed491cc
+# ╟─a05f7e2a-79ff-11eb-16f9-db358e178910
+# ╟─c657df0e-7a00-11eb-06e7-b1839a01a3b1
+# ╟─e8baffa4-7a00-11eb-3373-3d1dc1e6a952
+# ╟─55115388-7a01-11eb-2a9e-a769127bc90d
+# ╟─bcb82726-7a01-11eb-1b75-7136ad9adfb6
+# ╟─d33a25a0-7a01-11eb-35cc-573b861aadbb
+# ╟─0d718e70-7a02-11eb-27e0-0f145b163d55
+# ╟─7e1acfec-7a02-11eb-2b50-ff3f9e93545e
---
+++ Lectures/Lecture09.jl
@@ -0,0 +1,20 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c4a4bfe6-79fc-11eb-389d-fb0dda01299f
+begin
+	using PlutoUI
+	using NativeSVG
+end
+
+# ╔═╡ 6e3f9f5e-79fc-11eb-22ec-47b9055afb05
+md"""# Case Study: Word Play
+
+This chapter presents a second case study, which involves solving word puzzles by searching for words that have certain properties. For example, we’ll find the longest palindromes in English and search for words whose letters appear in alphabetical order. And I will present another program development plan: reduction to a previ‐ ously solved problem."""
+
+# ╔═╡ Cell order:
+# ╟─c4a4bfe6-79fc-11eb-389d-fb0dda01299f
+# ╟─6e3f9f5e-79fc-11eb-22ec-47b9055afb05
