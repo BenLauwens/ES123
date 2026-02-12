3ecfe1485c6877d679b644afa61d1b26e5e09278
Ben Lauwens <ben.lauwens@gmail.com>
2026-02-12 21:16:40 +0100

    Create 2025.jl

---
+++ projects/2025.jl
@@ -0,0 +1,88 @@
+### A Pluto.jl notebook ###
+# v0.20.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7cfa427a-9251-11eb-3b66-d313a367496d
+md"# Projects 2025"
+
+# ╔═╡ 4987617e-9252-11eb-0351-e76aec0c1812
+md"""## Groups
+
+### French speaking
+
+#### Group A
+
+
+#### Group B
+
+
+#### Group C
+
+
+#### Combinations
+
+10 groups of one person from group A, one person from group B and one person from group C
+
+### Dutch speaking
+
+#### Group A
+
+
+#### Group B
+
+
+#### Group C
+
+
+#### Combinations
+
+10 groups of one person from group A, one person from group B and one person from group C
+"""
+
+# ╔═╡ 6a896c5e-9253-11eb-2f93-a9faf3ba8385
+md"""## Games without hidden state
+
+* [Risk](https://en.wikipedia.org/wiki/Risk_(game))
+* [Monopoly](https://www.spelregels.eu/wp-content/uploads/2021/01/spelregels-monopoly.pdf)
+* [Catan]()
+* [Powergrid]()
+* [Bommen en granaten / 1000 bombes et grenades](https://www.bordspelblog.nl/speluitleg/1000-bommen-en-granaten-speluitleg)
+* [Konijnenhokken / Clapiers à lapins](https://www.spellenwijs.nl/konijnen-hokken-dobbelspel/)
+* [Lost Cities]()
+* [Regenwormen / Vers de terre]()
+* [Roi et Compagnie / Dobbelkoning]()
+* [Hotel]()
+* [Hive]()
+* [Ace of Aces](https://gametek.substack.com/p/all-about-that-ace)
+"""
+
+# ╔═╡ 924df842-9259-11eb-380c-61f6d9928b7c
+md"""## Daily Work
+
+* Implement the game Pluto or Vs Code
+* Include documentation (functions, types, ...) such that Live Docs in Pluto work
+* Prepare a Pluto notebook with demo
+* Explain the datastructures
+* Detail the created functions
+* Post your code and notebook on BelADL before presentation in group
+"""
+
+# ╔═╡ 0ed42ce4-925a-11eb-1555-ebecc574440a
+md"""## Exam
+
+* Implement a bot playing the game automatically
+* Based on your own strategy
+* Prepare a Pluto notebook with demo
+* Explain the implemented strategy
+* Detail the datastructures and the logic
+* Post your code and notebook on BelADL before the day of the exam (individual)
+"""
+
+# ╔═╡ Cell order:
+# ╟─7cfa427a-9251-11eb-3b66-d313a367496d
+# ╟─4987617e-9252-11eb-0351-e76aec0c1812
+# ╟─6a896c5e-9253-11eb-2f93-a9faf3ba8385
+# ╟─924df842-9259-11eb-380c-61f6d9928b7c
+# ╟─0ed42ce4-925a-11eb-1555-ebecc574440a
